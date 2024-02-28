8248ecf2b2b5339c8f952afe72c936ae0a96b986
jok <jok@kruso.dk>
2024-02-28 23:00:06 +0100

    chore: kv test

--- apps/web-astro/src/middleware/cache.ts
+++ apps/web-astro/src/middleware/cache.ts
@@ -14,30 +14,32 @@ export const memoryCache = defineMiddleware(async (req, next) => {
   if (!req.locals.runtime?.env) return await next();
 
   const { KV } = req.locals.runtime.env;
-  await KV.put(req.url.pathname, "Hello, world!");
-  console.log(KV);
+
   // Add a `cache` method to the `req.locals` object
   // that will allow us to set the cache duration for each page.
   req.locals.cache = (seconds: number = 60) => {
     ttl = seconds;
   };
 
-  const cached = cache.get(req.url.pathname);
+  const cached = (await KV.get(req.url.pathname, { type: "json" })) as any;
 
   if (cached && cached.expires > Date.now()) {
     return cached.response.clone();
   } else if (cached) {
-    cache.delete(req.url.pathname);
+    await cache.delete(req.url.pathname);
   }
 
   const response = await next();
 
   // If the `cache` method was called, store the response in the cache.
   if (ttl !== undefined) {
-    cache.set(req.url.pathname, {
-      response: response.clone(),
-      expires: Date.now() + ttl * 1000,
-    });
+    await KV.put(
+      req.url.pathname,
+      JSON.stringify({
+        response: response.clone(),
+        expires: Date.now() + ttl * 1000,
+      })
+    );
   }
 
   return response;
